6e2ad018e19986fe5f668c6f732defd1aa29ecb1
Yash Pathak <88139515+Yasssshhh@users.noreply.github.com>
2021-10-04 20:12:18 +0530

    Update questions.js

--- seeds/questions.js
+++ seeds/questions.js
@@ -50,4 +50,132 @@ module.exports = [
     statement: "What is material requirement planning??",
     category: "ce",
   },
+  {
+    statement: "What is a programming language???",
+    category: "cse",
+  },
+  {
+    statement: " Distinguish between constructor and method abstract class and interface",
+    category: "cse",
+  },
+  {
+    statement: " What is the difference between C and C++? ",
+    category: "cse",
+  },
+  {
+    statement: " List out different OOPS principles? ",
+    category: "cse",
+  },
+  {
+    statement: "  What is inheritance? ",
+    category: "cse",
+  },
+  {
+    statement: " What are the advantages and disadvantages of multiple inheritance? ",
+    category: "cse",
+  },
+  {
+    statement: "  What is the difference between overriding and overloading?",
+    category: "cse",
+  },
+  {
+    statement: "  What is a class? What is a superclass?",
+    category: "cse",
+  },
+  {
+    statement: "What is an operating system? What are the popular operating systems used today?",
+    category: "cse",
+  },
+  {
+    statement: "What are the primary components of a computer system?",
+    category: "cse",
+  },
+  {
+    statement: " What is a character stream? ",
+    category: "cse",
+  },
+  {
+    statement: " What is a chipset? ",
+    category: "cse",
+  },
+  {
+    statement: " What is an interface? ",
+    category: "cse",
+  },
+  {
+    statement: "  What is a file? ",
+    category: "cse",
+  },
+  {
+    statement: "   What is primary and secondary memory? ",
+    category: "cse",
+  },
+  {
+    statement: " What are the commonly used computer processors? ",
+    category: "cse",
+  },
+  {
+    statement: "Define Power Rating?",
+    category: "ece",
+  },
+  {
+    statement: "What is demodulation?",
+    category: "ece",
+  },
+  {
+    statement: "What is resistor?",
+    category: "ece",
+  },
+  {
+    statement: "What is a semi conductor?",
+    category: "ece",
+  },
+  {
+    statement: "What is modulation? And where it is utilized?",
+    category: "ece",
+  },
+  {
+    statement: "Is Ece hard?",
+    category: "ece",
+  },
+  {
+    statement: " What is latch up?",
+    category: "ece",
+  },
+  {
+    statement: "What is diode?",
+    category: "ece",
+  },
+  {
+    statement: "What is cut-off frequency?",
+    category: "ece",
+  },
+  {
+    statement: "What is a semi conductor?",
+    category: "ece",
+  },
+  {
+    statement: "What are the parts of Network Management System (NMS)?",
+    category: "ece",
+  },
+  {
+    statement: "How many satellites are required to cover the earth?",
+    category: "ece",
+  },
+  {
+    statement: "Example for negative feedback and positive feedback?",
+    category: "ece",
+  },
+  {
+    statement: "What is a feedback?",
+    category: "ece",
+  },
+  {
+    statement: "What are the main divisions of power system?",
+    category: "ece",
+  },
+  {
+    statement: "Explain different types of feedback?",
+    category: "ece",
+  },
 ];
