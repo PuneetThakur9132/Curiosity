ca71b554183ff1bd31eacb3f436fc3c95df78688
Yash Pathak <yashpathak6302@gmail.com>
2021-10-06 10:37:46 +0530

    Chnages

---
+++ Desktop/Curiosity/app.js
@@ -0,0 +1,52 @@
+// ************ NPM MODULES **********//
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const ejsMate = require("ejs-mate");
+const path = require("path");
+
+const Question = require("./models/question");
+const Answer = require("./models/answer");
+
+const ExpressError = require("./utils/ExpressError");
+const feedRoutes = require("./routes/feed");
+const userRoutes=require("./routes/auth")
+
+//****** CONNECTING DATABASE****** //
+mongoose.connect("mongodb://localhost:27017/Curiosity", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); // for serving static assets
+
+//********SETTING UP ROUTES *********//
+app.use(userRoutes);
+app.use(feedRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No , Something Went Wrong";
+  if (err.statusCode === 404) {
+    return res.status(statusCode).render("page404", { err });
+  }
+  res.status(statusCode).render("error", { err });
+});
+app.listen(3000, () => {
+  console.log("SERVING ON PORT 3000");
+});
