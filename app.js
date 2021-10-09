e1aa7b15baf83a6d7e22b4627572124b80dadd2b
Puneet Thakur <punsttakur9132@gmail.com>
2021-10-09 17:34:30 +0530

    fixing logged navbar

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
 const path = require("path");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
 const Question = require("./models/question");
 const Answer = require("./models/answer");
@@ -38,7 +38,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public"))); // for serving static assets
 
 // configuring session
@@ -72,6 +72,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
