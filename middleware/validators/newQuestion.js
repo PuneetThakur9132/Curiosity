b04ca11b9b5be9728969a306f4a9ed9c94b925db
Yash Pathak <yashpathak6302@gmail.com>
2021-10-12 13:48:28 +0530

    new and getQuestion validators

---
+++ middleware/validators/newQuestion.js
@@ -0,0 +1,33 @@
+const validator = require("validator");
+
+module.exports = (req, res, next) => {
+  try {
+    const statement = req.body.statement;
+    const category = req.body.category;
+
+    if (validator.isEmpty(statement)) {
+      req.flash("error", "Please add a valid question");
+      return req.redirect("/home");
+    }
+    //Checking category
+    if (
+      category !== "ece" &&
+      category !== "cse" &&
+      category !== "ee" &&
+      category !== "me" &&
+      category !== "ce" &&
+      category !== "ct" &&
+      category !== "cv" &&
+      category !== "lt" &&
+      category !== "ft" &&
+      category !== "ot"
+    ) {
+      req.flash("error", "Invalid category");
+      return res.redirect("/home");
+    }
+    //If validation is successfull
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
