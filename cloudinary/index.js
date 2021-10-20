8cbd0faeb9b9ebf6fc603831f9100866c0e78312
Yash Pathak <yashpathak6302@gmail.com>
2021-10-20 16:22:53 +0530

    Fixing Silly mistakes

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -4,9 +4,9 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.COULDINARY_CLOUD_NAME,
-  api_key: process.env.COULDINARY_API_KEY,
-  api_secret: process.env.COULDINARY_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 const storage = new CloudinaryStorage({
   cloudinary,
