c8e70dddab29aa06012eb45ea4635f81e25360be
Puneet Thakur <punsttakur9132@gmail.com>
2022-02-23 00:30:14 +0530

    changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const ExpressError = require("./utils/ExpressError");
 const feedRoutes = require("./routes/feed");
 const authRoutes = require("./routes/auth");
 const MongoStore = require('connect-mongo')(session);
-const dbUrl = "mongodb://localhost:27017/Curiosity";
+const dbUrl = process.env.DB_URL||"mongodb://localhost:27017/Curiosity";
 
 // Passport configuring
 
@@ -51,9 +51,10 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public"))); // for serving static assets
 
 // configuring session
+const secret = process.env.SECRET|| "thisshouldbeabettersecret!";
 const store =  new MongoStore({
   url:dbUrl,
-  secret: "thisshouldbeabettersecret!",
+  secret,
   touchAfter:24*60*60
 })
 
@@ -64,7 +65,7 @@ store.on("error",function(e){
 const sessionConfig = {
   store,
   name:'session',
-  secret: "thisshouldbeabettersecret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -115,6 +116,8 @@ app.use((err, req, res, next) => {
   }
   res.status(statusCode).render("error", { err });
 });
-app.listen(3000, () => {
-  console.log("SERVING ON PORT 3000");
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`SERVING ON PORT ${port}`);
 });
